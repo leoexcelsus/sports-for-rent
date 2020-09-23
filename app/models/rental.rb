e910bc6260c7a2085f247194ad9c147238cdd532
Leandro Sousa <leoexcelsus@gmail.com>
2020-09-23 18:27:28 -0300

    incluir 5 regras de validação para o modelo Rental

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,10 @@
 class Rental < ApplicationRecord
   belongs_to :product
   belongs_to :user
+
+  validates :begin_date, presence: true
+  validates :end_date, presence: true
+  validates :rental_price, presence: true
+  validates :product, presence: true
+  validates :user, presence: true
 end
