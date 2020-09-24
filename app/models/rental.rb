b1e9a0c32c2de14ff889f0de02e4a188511a020b
Leandro Sousa <leoexcelsus@gmail.com>
2020-09-24 11:57:49 -0300

    mais validações no modelo de Rental: begin_date e price

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -7,4 +7,14 @@ class Rental < ApplicationRecord
   validates :rental_price, presence: true
   validates :product, presence: true
   validates :user, presence: true
+
+  validates :price, numericality: { greater_than: 0 }
+  validate :end_date_must_be_greater_than_begin_date
+
+  def end_date_must_be_greater_than_begin_date
+    if begin_date > end_date && begin_date < Date.today
+      errors.add(:begin_date, "can't be in the past and must be before end date")
+    end
+  end
+
 end
