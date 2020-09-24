9317e580fb08b588a7d6d705ed261a68065074fa
Leandro Sousa <leoexcelsus@gmail.com>
2020-09-24 16:13:32 -0300

    Model already has lat long attributes: skipping migration creation. Add geocode to model Product.

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,9 @@ class Product < ApplicationRecord
   has_many :rentals
   has_one_attached :photo
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
+
   validates :description, presence: true
   validates :price, presence: true
   validates :address, presence: true
